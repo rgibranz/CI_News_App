bc06c1acfb7e227586ccf5e7ddf52b888ff884eb
rahmat gibran zulbadriansyah <rgibran234@gmail.com>
2021-04-29 18:32:20 +0700

    admin auth routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+// admin Login Routes
+$route['admin/login'] = 'authController/adminLogin';
+$route['admin/auth'] = 'authController/adminAuth';
