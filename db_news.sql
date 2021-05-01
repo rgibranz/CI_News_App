374374987a59ad4892db68ed1bb4cc9c1705f5a3
rahmat gibran zulbadriansyah <rgibran234@gmail.com>
2021-05-01 23:46:28 +0700

    add db sql

---
+++ db_news.sql
@@ -0,0 +1,35 @@
+CREATE TABLE `post` (
+  `post_id` int PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(255),
+  `slug` varchar(255),
+  `subject` varchar(255),
+  `image` varchar(255),
+  `created_at` timestamp,
+  `updated_at` timestamp,
+  `category` varchar(255),
+  `admin_id` int
+);
+
+CREATE TABLE `category` (
+  `id_category` int PRIMARY KEY AUTO_INCREMENT,
+  `category` varchar(255)
+);
+
+CREATE TABLE `admin` (
+  `admin_id` int PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(255),
+  `password` varchar(255)
+);
+
+CREATE TABLE `user` (
+  `user_id` int PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(255),
+  `password` varchar(255)
+);
+
+CREATE TABLE `comment` (
+  `comment_id` int PRIMARY KEY AUTO_INCREMENT,
+  `subject` varchar(255),
+  `user_id` int,
+  `post_id` int
+);
